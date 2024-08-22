ea411f00ce5bfddc46604efc6c137d82bf1303e7
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-08-22 07:21:52 +0100

    pick path param and init go module

--- cloudwatch_logging/server.go
+++ cloudwatch_logging/server.go
@@ -10,16 +10,17 @@ import (
 func main() {
 	// TODO implement a middleware
 	r := http.NewServeMux()
-	r.Handle("/api/:key", http.StripPrefix("/api/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// on golang 1.19 so no r.PathValue()
-		fmt.Fprintf(w, `{ "message": "Hello from %s and %s" }`, r.URL.Path, r.URL.RawPath)
-	})))
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{ "message": "healthy" }`)
 	})
 	r.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `{ "message": "Hello" }`)
 	})
+
+	r.Handle("/api/", http.StripPrefix("/api/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// on golang 1.19 so no r.PathValue()
+		fmt.Fprintf(w, `{ "message": "Hello", "path": "%s" }`, r.URL.Path)
+	})))
 	s := &http.Server{
 		Addr:        ":8080",
 		Handler:     r,
