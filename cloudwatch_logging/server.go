ba2fdb868b45545098b01f5ec38a1b93fb96894e
Idiakose O. Sunday <idiakosesunday@gmail.com>
2024-08-24 18:58:19 +0100

    add request logging

--- cloudwatch_logging/server.go
+++ cloudwatch_logging/server.go
@@ -4,23 +4,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
-func main() {
-	// TODO implement a middleware
-	r := http.NewServeMux()
-	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, `{ "message": "healthy" }`)
-	})
-	r.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, `{ "message": "Hello" }`)
-	})
+// wrapHandlerWithLogging adds logging to a handler
+// taken from https://gist.github.com/Boerworz/b683e46ae0761056a636#gistcomment-5153619
+func wrapHandlerWithLogging(h func(w http.ResponseWriter, r *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Printf("HTTP %s %s \n", req.Method, req.URL.Path)
 
-	r.Handle("/api/", http.StripPrefix("/api/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lrw := NewLoggingResponseWriter(w)
+
+		http.HandlerFunc(h).ServeHTTP(lrw, req)
+
+		log.Printf("HTTP %s %s %d\n", req.Method, req.URL.Path, lrw.statusCode)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func main() {
+	r := http.NewServeMux()
+
+	r.Handle("/health", wrapHandlerWithLogging(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{ "message": "healthy" }`)
+	}))
+	r.Handle("/api", wrapHandlerWithLogging(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{ "message": "Hello" }`)
+	}))
+	r.Handle("/api/", wrapHandlerWithLogging(func(w http.ResponseWriter, r *http.Request) {
 		// on golang 1.19 so no r.PathValue()
-		fmt.Fprintf(w, `{ "message": "Hello", "path": "%s" }`, r.URL.Path)
-	})))
+		fmt.Fprintf(w, `{ "message": "Hello", "path": "%s" }`, strings.TrimPrefix(r.URL.Path, "/api/"))
+	}))
+	r.Handle("/", wrapHandlerWithLogging(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		fmt.Fprintf(w, `{ "message", "Not Found" }`)
+	}))
+
 	s := &http.Server{
 		Addr:        ":8080",
 		Handler:     r,
